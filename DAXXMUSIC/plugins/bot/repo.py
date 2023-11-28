746eebea31ce41246459b157b0ddfb5098777862
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-11-28 08:44:10 +0530

    Update repo.py

--- DAXXMUSIC/plugins/bot/repo.py
+++ DAXXMUSIC/plugins/bot/repo.py
@@ -4,17 +4,48 @@ from DAXXMUSIC import app
 from config import BOT_USERNAME
 
 start_txt = """**
-lਤਕ‌ੜੇ💪 ਮੂਹਰੇ👉 ਕਦੇ ਜੀ ਜੀ🙏 ਨੀ ❌ਕੀਤਾ ਤੇ,
+ ਤਕ‌ੜੇ💪 ਮੂਹਰੇ👉 ਕਦੇ ਜੀ ਜੀ🙏 ਨੀ ❌ਕੀਤਾ ਤੇ,
 ਮਾੜੇ 😡ਨੂੰ ਦੇਖ👀 ਕੇ ਹੱਸਦੇ 😂ਨੀਂ,
 ਆਪਣੀ ☝🏻🌎ਦੁਨੀਆਂ👉 ਵਿੱਚ ਮਸਤ😎 ਰਹਿੰਦੇ ਆ।
 ਐਵੇਂ 😏ਕਿਸੇ 👉ਨੂੰ ਦੇਖ 👀ਕੇ ਮੱਚਦੇ 😅ਨੀ,
 ਜੋ 👉ਦਿਲ ♥️ਚ ਉਹੀ ਮੂੰਹ 🗣️ਤੇ ਆ,
 ਤਾਂ 👉ਹੀ ਬਹੁਤਿਆਂ👥 ਨੂੰ ਅਸੀਂ😎 ਜੱਚਦੇ😏 ਨੀ।।
-✍🏻 𝐌𝐲 𝐑𝐞𝐩𝐨 ➪ **[ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/ABT_BAD) 💞**\n📡 𝐌𝐚𝐝𝐞 𝐁𝐲 ➪ **[⎯꯭̽🇨🇦꯭꯭ ⃪Вα꯭∂ ꯭мυη∂α_꯭آآ꯭꯭꯭꯭⎯꯭ ꯭̽🌸](https://t.me/II_BAD_MUNDA_II)
+✍🏻 💞\n📡 𝐌𝐚𝐝𝐞 𝐁𝐲 ➪ [⎯꯭‌🇨🇦꯭꯭ ⃪Вα꯭∂ ꯭мυη∂α_꯭آآ꯭꯭꯭꯭⎯꯭ ꯭‌🌸](https://t.me/II_BAD_MUNDA_II)
 **"""
-  
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("𝗔𝗗𝗗 𝗠𝗘", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/THE_DRAMA_CLUB_01"),
+          InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/II_BAD_MUNDA_II"),
+          ],
+              
+
+              [
+              InlineKeyboardButton("𝗠𝗔𝗡𝗔𝗚𝗘𝗠𝗘𝗡𝗧︎", url=f"https://t.me/BAD_BBY_02_BOT"),
+              ],
+[
+InlineKeyboardButton("𝗦𝗧𝗥𝗜𝗡𝗚𝗕𝗢𝗧", url=f"https://t.me/BAD_STRING_SESSION_BOT"),
+
+],
+
+              [
+              InlineKeyboardButton("𝗦𝗧𝗥𝗜𝗡𝗚 𝗛𝗔𝗖𝗞︎", url=f"BAD_STRING_SESSION_HACK_BOT"),
+              ],
+
+]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
     await msg.reply_photo(
-      photo="https://te.legra.ph/file/e25a563619c653328830d.jpg",
+        photo="https://te.legra.ph/file/e25a563619c653328830d.jpg",
         caption=start_txt,
         reply_markup=reply_markup
-                         )
+    )
