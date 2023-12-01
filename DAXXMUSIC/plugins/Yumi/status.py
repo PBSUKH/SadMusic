f3fbf8a8074b936b7bee17282ed129036fad143d
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-01 21:02:36 +0530

    Add files via upload

---
+++ DAXXMUSIC/plugins/Yumi/status.py
@@ -0,0 +1,50 @@
+from pyrogram import Client, filters
+from DAXXMUSIC import app
+
+
+# Command handler to get group status
+@app.on_message(filters.command("status") & filters.group)
+def group_status(client, message):
+    chat = message.chat  # Chat where the command was sent
+    status_text = f"Group ID: {chat.id}\n" \
+                  f"Title: {chat.title}\n" \
+                  f"Type: {chat.type}\n"
+                  
+    if chat.username:  # Not all groups have a username
+        status_text += f"Username: @{chat.username}"
+    else:
+        status_text += "Username: None"
+
+    message.reply_text(status_text)
+
+
+#########
+
+""" ***                                                                       
+────────────────────────────────────────────────────────────────────────
+─████████████────██████████████──████████──████████──████████──████████─
+─██░░░░░░░░████──██░░░░░░░░░░██──██░░░░██──██░░░░██──██░░░░██──██░░░░██─
+─██░░████░░░░██──██░░██████░░██──████░░██──██░░████──████░░██──██░░████─
+─██░░██──██░░██──██░░██──██░░██────██░░░░██░░░░██──────██░░░░██░░░░██───
+─██░░██──██░░██──██░░██████░░██────████░░░░░░████──────████░░░░░░████───
+─██░░██──██░░██──██░░░░░░░░░░██──────██░░░░░░██──────────██░░░░░░██─────
+─██░░██──██░░██──██░░██████░░██────████░░░░░░████──────████░░░░░░████───
+─██░░██──██░░██──██░░██──██░░██────██░░░░██░░░░██──────██░░░░██░░░░██───
+─██░░████░░░░██──██░░██──██░░██──████░░██──██░░████──████░░██──██░░████─
+─██░░░░░░░░████──██░░██──██░░██──██░░░░██──██░░░░██──██░░░░██──██░░░░██─
+─████████████────██████──██████──████████──████████──████████──████████─
+────────────────────────────────────────────────────────────────────────**"""
+
+
+
+####
+@app.on_message(filters.command('groupinfo') & filters.group)
+def group_info(client, message):
+    chat_id = message.chat.id
+    chat_info = app.get_chat(chat_id)
+    title = chat_info.title
+    description = chat_info.description
+    member_count = chat_info.members_count
+
+    info_text = f"Group Title: {title}\nDescription: {description}\nMembers Count: {member_count}"
+    message.reply_text(info_text)
