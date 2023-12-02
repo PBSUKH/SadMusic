11fe64c91267dca58ecab665f5b1693eb6e0b4d2
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-02 10:51:41 +0530

    Update badban.py

--- DAXXMUSIC/plugins/sudo/badban.py
+++ DAXXMUSIC/plugins/sudo/badban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["bad baby","bby"], prefixes=["y", "Y"]) & admin_filter)
+@app.on_message(filters.command(["Bad baby","Bby"], prefixes=["B", "B"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
