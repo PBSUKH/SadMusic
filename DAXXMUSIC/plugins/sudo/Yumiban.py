639659c818180b5d79d00f777041406c03e348e4
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-05 21:25:19 +0530

    Update Yumiban.py

--- DAXXMUSIC/plugins/sudo/Yumiban.py
+++ DAXXMUSIC/plugins/sudo/Yumiban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["hizu"], prefixes=["s", "S"]) & admin_filter)
+@app.on_message(filters.command(["adbaby"], prefixes=["b", "B"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
