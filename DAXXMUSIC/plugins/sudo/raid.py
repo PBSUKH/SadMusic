1389cb79c6fc52c1edbfc2759e661d11d3779a6d
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-19 14:22:13 +0530

    Update raid.py

--- DAXXMUSIC/plugins/sudo/raid.py
+++ DAXXMUSIC/plugins/sudo/raid.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 from DAXXMUSIC.misc import SUDOERS
 
 # Define the command handler
-@app.on_message(filters.command(".raid", prefixes=".")  & SUDOERS)
+@app.on_message(filters.command("raid", prefixes=".")  & SUDOERS)
 def repeat_message(client, message):
     # Get the text following the .raid command
     command_args = message.text.split(".raid", 1)[-1].strip()
