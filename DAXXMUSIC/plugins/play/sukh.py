97969e6996a224ce07e825a6394317536120860c
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-31 14:22:18 +0530

    Update sukh.py

--- DAXXMUSIC/plugins/play/sukh.py
+++ DAXXMUSIC/plugins/play/sukh.py
@@ -36,7 +36,7 @@ def apply_bass_boost(audio_path):
     audio = AudioSegment.from_file(audio_path)
 
     # Apply bass boost effect (adjust the gain according to your preference)
-    boosted_audio = audio.low_pass_filter(800).high_pass_filter(50).apply_gain(10)
+    boosted_audio = audio.low_pass_filter(800).high_pass_filter(10).apply_gain(10)
 
     # Save the boosted audio as a temporary file
     boosted_audio_path = "вα∂ мυη∂α вαѕѕ.mp3"
