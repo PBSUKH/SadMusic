dbb4485b5500ef533d40dcd4eba82f5a2c69a166
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-31 15:07:50 +0530

    Update and rename memify.py to mmf.py

--- DAXXMUSIC/plugins/tools/mmf.py
+++ DAXXMUSIC/plugins/tools/mmf.py
@@ -1,79 +1,112 @@
 import os
 import textwrap
-from PIL import Image, ImageDraw, ImageFont
-from DAXXMUSIC import app as app
-from pyrogram import Client, filters, types
-from pyrogram.types import Message
-from pyrogram.raw.types import InputFile
-from io import BytesIO
-from pyrogram.raw.functions.messages import GetStickerSet
 
-TEMP_DOWNLOAD_DIRECTORY = []
+from PIL import Image, ImageDraw, ImageFont
+
+from DAXXMUSIC import telethn as bot
+from DAXXMUSIC.events import register
+
 
 @app.on_message(filters.command("mmf"))
 async def handler(event):
+
     if event.fwd_from:
+
         return
+
     if not event.reply_to_msg_id:
-        await event.reply("`Provide Some Text To Draw! And Reply To Image/Stickers EXAMPLE: /mmf text`")
+
+        await event.reply("ᴘʀᴏᴠɪᴅᴇ ꜱᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴅʀᴀᴡ ʙᴀʙʏ🥀!")
+
         return
+
     reply_message = await event.get_reply_message()
+
     if not reply_message.media:
-        await event.reply("```Reply to a image/sticker.```")
+
+        await event.reply("```ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ/ꜱᴛɪᴄᴋᴇʀ ʙᴀʙʏ🥀.```")
+
         return
+
     file = await bot.download_media(reply_message)
-    msg = await event.reply("```Memifying this image! (」ﾟﾛﾟ)｣ ```")
+
+    msg = await event.reply("```ᴍᴇᴍɪꜰʏɪɴɢ ᴛʜɪꜱ ɪᴍᴀɢᴇ ʙᴀʙʏ🥀! ```")
+
+
     text = str(event.pattern_match.group(1)).strip()
+
     if len(text) < 1:
-        return await msg.edit("You might want to try `/mmf text`")
+
+        return await msg.reply("ʏᴏᴜ ᴍɪɢʜᴛ ᴡᴀɴᴛ ᴛᴏ ᴛʀʏ `/mmf text` ʙᴀʙʏ🥀")
+
     meme = await drawText(file, text)
+
     await bot.send_file(event.chat_id, file=meme, force_document=False)
+
     await msg.delete()
+
     os.remove(meme)
 
 
-# Taken from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/memify.py#L64
-# Maybe replyed to suit the needs of this module
-
-
 async def drawText(image_path, text):
+
     img = Image.open(image_path)
+
     os.remove(image_path)
-    shadowcolor = "black"
+
     i_width, i_height = img.size
+
     if os.name == "nt":
+
         fnt = "ariel.ttf"
+
     else:
-        fnt = "./EmikoRobot/resources/default.ttf"
+
+        fnt = "./IRO/IROFont/logofonts/default.ttf"
+
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
+
     if ";" in text:
+
         upper_text, lower_text = text.split(";")
+
     else:
+
         upper_text = text
+
         lower_text = ""
+
     draw = ImageDraw.Draw(img)
+
     current_h, pad = 10, 5
+
     if upper_text:
+
         for u_text in textwrap.wrap(upper_text, width=15):
+
             u_width, u_height = draw.textsize(u_text, font=m_font)
+
             draw.text(
                 xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
                 text=u_text,
@@ -87,10 +120,15 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(255, 255, 255),
             )
+
             current_h += u_height + pad
+
     if lower_text:
+
         for l_text in textwrap.wrap(lower_text, width=15):
+
             u_width, u_height = draw.textsize(l_text, font=m_font)
+
             draw.text(
                 xy=(
                     ((i_width - u_width) / 2) - 2,
@@ -100,6 +138,7 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(
                     ((i_width - u_width) / 2) + 2,
@@ -109,6 +148,7 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(
                     (i_width - u_width) / 2,
@@ -118,6 +158,7 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(
                     (i_width - u_width) / 2,
@@ -137,8 +178,16 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(255, 255, 255),
             )
+
             current_h += u_height + pad
+
     image_name = "memify.webp"
+
     webp_file = os.path.join(image_name)
+
     img.save(webp_file, "webp")
+
     return webp_file
+
+
+__mod_name__ = "mmf"
