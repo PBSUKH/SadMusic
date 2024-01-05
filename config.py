325973923063a692161564e9b905251cc7443918
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-05 20:01:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -90,7 +90,7 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
+TELEGRAM_AUDIO_URL = ""https://telegra.ph/file/e575ae40d6635250974e1.jpg""
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
 STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
 SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
