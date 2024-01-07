10ce126c54c77a4613c98e22f49618bd401b7de3
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-07 11:22:43 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -4,7 +4,7 @@ from DAXXMUSIC import app
 from pyrogram.types import InputMediaPhoto
 
 
-@app.on_message(filters.command(["tgm" , "link"]))
+@app.on_message(filters.command(["tgm" , "telegraph"]))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
@@ -14,5 +14,18 @@ def ul(_, message):
         for x in fk:
             url = "https://telegra.ph" + x
 
-        i.edit(f'link {url}')
-        
+        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
+
+########____________________________________________________________######
+
+@app.on_message(filters.command(["graph" , "grf"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://graph.org" + x
+
+        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
